bd199ceba24349e1f24ce24f8eefd96472b606ec
Alejo Greco <uki.greco@gmail.com>
2022-08-06 20:33:48 -0300

    Basic routes creation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Register from './pages/Register';
+import Welcome from './pages/Welcome';
 
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Welcome />}/>
+        <Route path='/home' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/register' element={<Register />}/>
+        <Route path='/subscript' element={<NotFound />}/>
+        <Route path='*' element={<NotFound />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
