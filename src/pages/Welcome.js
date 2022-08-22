6c18c8760d32e638052244b15b7b79a9218b6409
Alejo Greco <uki.greco@gmail.com>
2022-08-22 17:04:56 -0300

    Button on wellcome page for testing

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from '@mui/material'
 
 const Welcome = () => {
   return (
-    <div>Welcome</div>
+    <Button variant='contained'>
+      <Link to='/home'>GET STARTED</Link>
+    </Button>
   )
 }
 
