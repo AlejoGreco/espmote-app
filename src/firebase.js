3be02dbe8ed041135291eb7c9b077edb39c18dc6
Alejo Greco <uki.greco@gmail.com>
2022-08-24 13:27:05 -0300

    Config app connection with firebase database

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
+import { getDatabase } from "firebase/database";
+
 //import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -16,11 +18,12 @@ const firebaseConfig = {
   storageBucket: "espmote.appspot.com",
   messagingSenderId: "309598747416",
   appId: "1:309598747416:web:c322f28d76bf803cb17c6d",
-  measurementId: "G-F1G4KEK6EP"
+  measurementId: "G-F1G4KEK6EP",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const database = getDatabase(app);
 //const analytics = getAnalytics(app);
 
