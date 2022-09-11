bc2e1858e8d9245bbe6a6161c05b4f3cee936717
Alejo Greco <uki.greco@gmail.com>
2022-09-10 22:45:41 -0300

    Adding alarm card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,11 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getUserNodeIds, setNodeData } from '../store/slices/nodes'
 import { onValue, ref } from 'firebase/database'
 import { database } from '../firebase'
-import { Grid, Typography } from '@mui/material'
+import { Grid } from '@mui/material'
 import PageFrame from '../components/PageFrame'
 import FormNode from '../components/FormNode'
 import ListNodeContainer from '../components/ListNodeContainer'
+import AlarmNoteCard from '../components/cards/AlarmNoteCard'
 
 const Home = () => {
     const dispatch = useDispatch()
@@ -45,7 +46,7 @@ return (
             <Grid item xs={12} sm={12} md={12} lg={4} xl={4}>
                 <Grid container justifyContent='center' spacing={3}>
                     <Grid item xs={12} sm={8} md={5} lg={12} xl={12}>
-                        <Typography>Aviso alarmas nuevas</Typography>
+                        <AlarmNoteCard />
                     </Grid>
                     <Grid item xs={12} sm={8} md={5} lg={12} xl={12}>
                         <FormNode />
