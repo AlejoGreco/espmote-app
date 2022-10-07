0c10641f1476e2973d489a9756354455e916235a
Alejo Greco <uki.greco@gmail.com>
2022-10-07 20:27:42 -0300

    Chart with dummy data

--- src/pages/NodeDetails.js
+++ src/pages/NodeDetails.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { Grid } from '@mui/material'
 import PageFrame from '../components/PageFrame'
+import NodeDetailChart from '../components/charts/NodeDetailChart'
 
 const NodeDetails = () => {
     const { id } = useParams()
     return (
         <PageFrame>
-            <h2>{`Node ${id} details`}</h2>
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                    <h2>{`Node ${id} details con grid`}</h2>
+                    <NodeDetailChart />
+                </Grid>
+            </Grid>
         </PageFrame>
     )
 }
