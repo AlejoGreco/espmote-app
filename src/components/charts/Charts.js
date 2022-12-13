ffbf9fe11290d7b56e6846d96c6caacd854bb5d2
Alejo Greco <uki.greco@gmail.com>
2022-12-12 23:52:46 -0300

    Basic chart works

--- src/components/charts/Charts.js
+++ src/components/charts/Charts.js
@@ -1,61 +1,41 @@
 //import { useTheme } from '@emotion/react'
-//import CustomChart from './CustomChart'
 import Chart from "react-apexcharts";
-import { getChartTime, getChartSeries, getChartOptions } from '../../utils'
+import { getChartSeries, getChartOptions } from '../../utils'
 
 
 const Charts = ({nodeType, allNodeData, nodeLabels}) => {
     // Tipos de node 1: humedad - 2: temperatura - 3: mixto(temp + hum) - 4: silo(temp + hum + co2) - 5: indor()
     //const theme = useTheme()
-    const labels = getChartTime(nodeLabels)
-    const keys = Object.keys(allNodeData[0]).filter(k => k !== 'type')
-    const uniqueKeys = [...new Set(keys.map(k => k.slice(0, -1)))]
+    //const keys = Object.keys(allNodeData[0]).filter(k => k !== 'type')
+    //const uniqueKeys = [...new Set(keys.map(k => k.slice(0, -1)))]
 
     switch (nodeType){
         case '1':
         case '2':
         case '3':
-            console.log({options: getChartOptions(nodeType, uniqueKeys)})
-            console.log({...getChartSeries(allNodeData, nodeLabels)})
             return (
                 <Chart 
-                    series={getChartSeries(allNodeData, labels).series}
                     type='line'
-                    options={{xaxis: {type: 'datetime'}}}
+                    options={getChartOptions()}
+                    series={getChartSeries(allNodeData, nodeLabels)}
                 />
-                /*<CustomChart 
-                    options={getChartOptions(nodeType, uniqueKeys)}
-                    data={{
-                        labels, 
-                        datasets: getChartDatasets(allNodeData, theme)
-                    }} 
-                />*/
             )
         case '4':
             const co2Data = allNodeData.map(data => data.co20)
             const rest = allNodeData.map(data => ({hum0: data.hum0, temp0: data.temp0}))
             return (
-                <div>
-                    {{dataCO2: getChartSeries(co2Data)}}
-                    <hr />
-                    {{data: getChartSeries(rest)}}
-                </div>
-                /*<>
-                    <CustomChart 
-                        options={getChartOptions('2', uniqueKeys)}
-                        data={{
-                            labels, 
-                            datasets: getChartDatasets(rest, theme)
-                        }} 
+                <>
+                    <Chart 
+                        type='line'
+                        options={getChartOptions()}
+                        series={getChartSeries(co2Data, nodeLabels)}
                     />
-                    <CustomChart 
-                        options={getChartOptions('4', uniqueKeys)}
-                        data={{
-                            labels, 
-                            datasets: getChartDatasets(co2Data, theme)
-                        }} 
+                    <Chart 
+                        type='line'
+                        options={getChartOptions()}
+                        series={getChartSeries(rest, nodeLabels)}
                     />
-                </>*/
+                </>
             )
         default:
             return  <h2>Error - tipo invalido de nodo - Type: {nodeType}</h2>    
--- src/utils/charts.js
+++ src/utils/charts.js
@@ -1,79 +1,27 @@
-import { variablesNames, variablesUnits } from '../constants'
+const getChartTime = timeArray => timeArray.map(t => new Date(parseInt(t)).getTime())
 
-export const chartLinesColors = (theme, key) => {
-    const colors = {
-        temp: {
-            borderColor: [
-                theme.palette.error.dark,
-                theme.palette.orange.dark,
-                theme.palette.warning.dark,
-                theme.palette.secondary.main
-            ],
-            backgroundColor: [
-                theme.palette.error.light,
-                theme.palette.orange.light,
-                theme.palette.warning.light,
-                theme.palette.secondary.light
-            ]
-        },
-        hum: {
-            borderColor: [
-                theme.palette.primary[800],
-                theme.palette.secondary[800],
-                theme.palette.success.dark,
-                theme.palette.primary.main,
-            ],
-            backgroundColor: [
-                theme.palette.primary.light,
-                theme.palette.secondary.light,
-                theme.palette.success.light,
-                theme.palette.primary.light,
-            ]
-        },
-        co2: {
-            borderColor: [
-                theme.palette.success.dark,
-                theme.palette.warning.dark,
-                theme.palette.secondary.dark,
-                theme.palette.primary.dark,
-            ],
-            backgroundColor: [
-                theme.palette.success.light,
-                theme.palette.warning.light,
-                theme.palette.secondary.light,
-                theme.palette.primary.light
-            ]
-        },
-        level: {
-            borderColor: [
-                theme.palette.primary[800],
-                theme.palette.secondary[800],
-                theme.palette.success.dark,
-                theme.palette.primary.main,
-            ],
-            backgroundColor: [
-                theme.palette.primary.light,
-                theme.palette.secondary.light,
-                theme.palette.success.light,
-                theme.palette.primary.light,
-            ]
-        }
-    }
-
-    return colors[key]
-}
-
-export const getChartTime = timeArray => timeArray.map(t => new Date(parseInt(t)).toLocaleDateString())
-
-const getChartDataSeries = (dataArray, timeArray) => {
+const getChartDataSerie = (dataArray, timeArray) => {
     const dataChart = dataArray.map((value, index) => ({x: timeArray[index], y: value }))
-    return {data: dataChart}
+    return dataChart
 }
 
 export const getChartSeries = (dataArray, timeArray) => {
     const dataKeys = Object.keys(dataArray[0]).filter(k => k !== 'type')
-    const series = dataKeys.map(key => getChartDataSeries(dataArray.map(obj => obj[key]), timeArray))
-    return { series }
+    const series = dataKeys.map(key => ({
+        name: key, 
+        data: getChartDataSerie(dataArray.map(obj => obj[key]), getChartTime(timeArray))
+    }))
+    return series
+}
+
+export const getChartOptions = () => {
+    const options = {
+        xaxis: {
+            type: 'datetime'
+        }
+    }
+
+    return options
 }
 
 /*export const getChartDatasets = (dataArray, timeArray) => {
@@ -95,68 +43,3 @@ export const getChartSeries = (dataArray, timeArray) => {
     })
     return datasets
 }*/
-
-
-export const getChartOptions = (type, keys) => {
-    const title = {
-        1: 'Humedad vs Tiempo',
-        2: 'Temperatura vs Tiempo',
-        3: 'Humedad y Temperatura vs Tiempo',
-        4: 'Consentracion Co2 vs Tiempo',
-        5: 'Varios vs Tiempo'
-    }
-
-    const scales = (keys.length > 1) 
-        ? {
-            y0: {
-                type: 'linear',
-                display: true,
-                position: 'left',
-                ticks: {
-                    callback: function(value, index, ticks) {
-                        return value + variablesUnits[keys[0]];
-                    }
-                }
-            },
-            y1: {
-                type: 'linear',
-                display: true,
-                position: 'right',
-                grid: {
-                    drawOnChartArea: false,
-                },
-                ticks: {
-                    callback: function(value, index, ticks) {
-                        return value + variablesUnits[keys[1]];
-                    }
-                }
-            }
-        } 
-        : {
-            y0: {
-                type: 'linear',
-                display: true,
-                position: 'left',
-                ticks: {
-                    callback: function(value, index, ticks) {
-                        return value + variablesUnits[keys[0]];
-                    }
-                }
-            }
-        }
-
-    const options = {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: title[type]
-            }
-        },
-        scales
-    }
-    return  options
-}
