ce5d0f7dc23db2a7ecdcc0565ea05e883aac7126
Alejo Greco <uki.greco@gmail.com>
2023-01-01 21:19:57 -0300

    Finish buttons style in chart control component

---
+++ src/components/BasicIconButton.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useTheme } from '@mui/material/styles';
+import {
+    Avatar,
+    ButtonBase,
+} from '@mui/material'
+
+
+const BasicIconButton = ({Icon}) => {
+    const theme = useTheme()
+
+  return (
+    <ButtonBase sx={{ borderRadius: '12px' }}>
+        <Avatar
+            variant="rounded"
+            sx={{
+                ...theme.typography.commonAvatar,
+                ...theme.typography.mediumAvatar,
+                transition: 'all .2s ease-in-out',
+                background: theme.palette.secondary.light,
+                color: theme.palette.secondary.dark,
+                '&[aria-controls="menu-list-grow"],&:hover': {
+                    background: theme.palette.secondary.dark,
+                    color: theme.palette.secondary.light
+                }
+            }}
+            aria-haspopup="true"
+            color="inherit"
+        >
+            <Icon stroke={1.5} size="1.3rem" />
+        </Avatar>
+    </ButtonBase>
+  )
+}
+
+export default BasicIconButton
--- src/components/charts/Controls.js
+++ src/components/charts/Controls.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Grid, Typography, TextField, MenuItem } from '@mui/material'
 import MainCard from '../cards/MainCard'
+import BasicIconButton from '../BasicIconButton'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import SearchIcon from '@mui/icons-material/Search'
+import CachedIcon from '@mui/icons-material/Cached';
 
 const status = [
     {
@@ -25,7 +29,7 @@ export const Controls = ({node}) => {
                 <Grid item>
                     <Grid container direction="column" spacing={1}>
                         <Grid item>
-                            <Typography variant="subtitle2">Nodo {node.id}</Typography>
+                            <Typography variant="subtitle2">Id {node.id}</Typography>
                         </Grid>
                         <Grid item>
                             <Typography variant="h4">{node.name}</Typography>
@@ -33,17 +37,36 @@ export const Controls = ({node}) => {
                     </Grid>
                 </Grid>
                 <Grid item>
-                    <TextField
-                        select
-                        value={'today'}
-                        onChange={e => {}}
-                    >
-                        {status.map((option) => (
-                            <MenuItem key={option.value} value={option.value}>
-                                {option.label}
-                            </MenuItem>
-                        ))}
-                    </TextField>
+                    <Grid container alignItems="center" justifyContent="space-between" gap={2}>
+                        <Grid item>
+                            <TextField
+                                select
+                                color='secondary'
+                                value={'today'}
+                                onChange={e => {}}
+                                sx={{
+                                    '& .MuiSelect-select': {
+                                        py: '.5rem !important'
+                                    }
+                                }}
+                            >
+                                {status.map((option) => (
+                                    <MenuItem key={option.value} value={option.value}>
+                                        {option.label}
+                                    </MenuItem>
+                                ))}
+                            </TextField>
+                        </Grid>
+                        <Grid item>
+                            <BasicIconButton Icon={SearchIcon} />
+                        </Grid>
+                        <Grid item>
+                            <BasicIconButton Icon={CachedIcon} />
+                        </Grid>
+                        <Grid item>
+                            <BasicIconButton Icon={ArrowBackIcon} />
+                        </Grid>
+                    </Grid>                    
                 </Grid>
             </Grid>             
         </MainCard>
